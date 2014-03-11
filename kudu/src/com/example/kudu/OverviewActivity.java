0b87b27271b3873cbe8d8f859a98577037d7e8a4
VChow <victorchow_507@hotmail.com>
2014-03-11 21:16:09 +0000

    Created classes and xml for all activities. Added xml layout for Profile Activity. Circular Dependency error!

---
+++ kudu/src/com/example/kudu/OverviewActivity.java
@@ -0,0 +1,22 @@
+package com.example.kudu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class OverviewActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_overview);
+
+	}
+	
+}
