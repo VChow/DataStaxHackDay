e3e9b96f9b77110883e47726ca0651026212efd0
VChow <victorchow_507@hotmail.com>
2014-03-11 19:54:38 +0000

    Minor updates to Register page

---
+++ kudu/src/com/example/kudu/RegisterActivity.java
@@ -0,0 +1,38 @@
+package com.example.kudu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class RegisterActivity extends Activity {
+
+	private Button btnRegister;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		
+		final ImageView imgView;
+		
+		imgView=(ImageView)findViewById(R.id.logo);
+		imgView.setImageResource(R.drawable.login_logo);
+	}
+	
+	public void setRegisterButtonListener(){
+		btnRegister = (Button) findViewById(R.id.register);
+		btnRegister.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			
+		}
+		});
+	}
+}
