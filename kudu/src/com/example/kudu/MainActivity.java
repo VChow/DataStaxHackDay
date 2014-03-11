e1af4afc82c366f9e6c9cfefc2deb76953a05f65
VChow <victorchow_507@hotmail.com>
2014-03-11 18:24:13 +0000

    Added Login page - Text fields + Buttons

--- kudu/src/com/example/kudu/MainActivity.java
+++ kudu/src/com/example/kudu/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.kudu;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private Button btnLogin;
+	private Button btnRegister;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final ImageView imgView;
+		
+		imgView=(ImageView)findViewById(R.id.logo);
+		imgView.setImageResource(R.drawable.xxhdpi_logo);
 	}
 
 	@Override
@@ -19,4 +32,26 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void setLoginButtonListener(){
+		btnLogin = (Button) findViewById(R.id.login);
+		btnLogin.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Toast.makeText(MainActivity.this,"Get Tae Fuck",Toast.LENGTH_LONG).show();		
+		}
+		});
+	}
+	
+	public void setRegisterButtonListener(){
+		btnLogin = (Button) findViewById(R.id.register);
+		btnLogin.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Toast.makeText(MainActivity.this,"Get Yoor Tits Oot",Toast.LENGTH_LONG).show();		
+		}
+		});
+	}
+	
 }
