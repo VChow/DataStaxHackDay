dfa32184778be26cc42892bdd9cec7b0e7840771
mmgoddard <mmgoddard@dundee.ac.uk>
2014-03-12 12:05:31 +0000

    added error message in RegisterActivity.java.

--- kudu/src/com/example/kudu/RegisterActivity.java
+++ kudu/src/com/example/kudu/RegisterActivity.java
@@ -2,14 +2,11 @@ package com.example.kudu;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
 
@@ -30,18 +27,27 @@ public class RegisterActivity extends Activity {
 		btnRegister = (Button) findViewById(R.id.register);
 		btnRegister.setOnClickListener(new OnClickListener() {
 		
-		@Override
-		public void onClick(View v) {
-			EditText usernameEditText = (EditText)findViewById(R.id.username);
-			EditText emailEditText = (EditText)findViewById(R.id.email);
-			EditText password1EditText = (EditText)findViewById(R.id.password_1);
-			EditText password2EditText = (EditText)findViewById(R.id.password_2);
-			String btnUsernameStr = usernameEditText.getText().toString();
-			String btnEmailStr = emailEditText.getText().toString();
-			String btnPassword1Str = password1EditText.getText().toString();
-			String btnPassword2Str = password2EditText.getText().toString();
+			@Override
+			public void onClick(View v) {
+				EditText usernameEditText = (EditText)findViewById(R.id.username);
+				EditText emailEditText = (EditText)findViewById(R.id.email);
+				EditText password1EditText = (EditText)findViewById(R.id.password_1);
+				EditText password2EditText = (EditText)findViewById(R.id.password_2);
+				String btnUsernameStr = usernameEditText.getText().toString();
+				String btnEmailStr = emailEditText.getText().toString();
+				String btnPassword1Str = password1EditText.getText().toString();
+				String btnPassword2Str = password2EditText.getText().toString();
 			
-		}
+				if(btnPassword1Str.equals(btnPassword2Str)) {
+					User user = new User();
+					user.setUsername(btnUsernameStr);
+					user.setEmail(btnEmailStr);
+					user.setPassword(btnPassword1Str);
+				}
+				else {
+					password2EditText.setError("The two password's do not match.");
+				}	
+			}
 		});
 	}
 }
