e4590a075d39644e924ee948908329a0340e237d
mmgoddard <mmgoddard@dundee.ac.uk>
2014-03-12 09:54:19 +0000

    finished login ui.

--- kudu/src/com/example/kudu/MainActivity.java
+++ kudu/src/com/example/kudu/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.kudu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -24,6 +26,9 @@ public class MainActivity extends Activity {
 		
 		imgView=(ImageView)findViewById(R.id.logo);
 		imgView.setImageResource(R.drawable.login_logo);
+		
+		setLoginButtonListener();
+		setRegisterButtonListener();
 	}
 
 	@Override
@@ -37,10 +42,19 @@ public class MainActivity extends Activity {
 		btnLogin = (Button) findViewById(R.id.login);
 		btnLogin.setOnClickListener(new OnClickListener() {
 		
-		@Override
-		public void onClick(View v) {
-			
-		}
+			@Override
+			public void onClick(View v) {
+				EditText usernameEditText = (EditText)findViewById(R.id.username);
+				EditText passwordEditText = (EditText)findViewById(R.id.password);
+				String btnUsernameStr = usernameEditText.getText().toString();
+				String btnPasswordStr = passwordEditText.getText().toString();
+				
+				//Used for testing
+				Toast.makeText(MainActivity.this,
+						"On Button Click : " + "\n" + 
+						btnUsernameStr + "\n" + 
+					    btnPasswordStr, Toast.LENGTH_LONG).show();
+			}
 		});
 	}
 	
@@ -48,10 +62,11 @@ public class MainActivity extends Activity {
 		btnRegister = (Button) findViewById(R.id.register);
 		btnRegister.setOnClickListener(new OnClickListener() {
 		
-		@Override
-		public void onClick(View v) {
-			
-		}
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
+				MainActivity.this.startActivity(myIntent);
+			}
 		});
 	}
 	
