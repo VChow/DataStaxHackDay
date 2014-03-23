f90afa777097fcc54c65c5f86e1dcea0e053df42
mmgoddard <mmgoddard@dundee.ac.uk>
2014-03-23 12:08:36 +0000

    fixed conflicts and added profile code

--- kudu/src/com/kudu/activities/ContactsActivityFragment.java
+++ kudu/src/com/kudu/activities/ContactsActivityFragment.java
@@ -2,21 +2,9 @@ package com.kudu.activities;
 
 
 import android.app.ListFragment;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import android.app.Fragment;
-import android.content.Context;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
@@ -26,9 +14,6 @@ public class ContactsActivityFragment extends ListFragment {
             "Linux", "OS/2","shit", "fuck", "shithouse", "twatend"};
     
 	public ContactsActivityFragment() {}
-	private UUID user_id;
-	public List contactsList = new ArrayList<String>();
-	public ContactsModel userContacts= new ContactsModel();
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,51 +23,4 @@ public class ContactsActivityFragment extends ListFragment {
         setListAdapter(adapter);
         return rootView;
     }
-	
-    /*
-     * Get a List of contacts for the user
-     */
-	public void retrieveContacts() {
-		//ContactsModel userContacts = new ContactsModel(user_id);
-		 
-		try 
-		{
-			contactsList = userContacts.retrieveContacts();
-		} 
-		catch (IllegalStateException e) {e.printStackTrace();} 
-		catch (IOException e) {e.printStackTrace();} 
-		catch (JSONException e) {e.printStackTrace();}
-		
-	}
-	
-	/*
-	 * Add a new contact to user's contact list
-	 */
-	public void addContact() {
-		//Once a contact is added, refresh the page?
-	}
-
-	/*
-	 * For each item in List, add a new clickable thing to UI
-	 */
-	public void populateContactsScreen() {
-		
-		for(int i = 0; i < contactsList.size(); i++){
-			//Read something about using an adapter
-			//to add new objects to UI
-		}
-	}
-	
-	private boolean checkInternetConnection() {
-		ConnectivityManager conMgr = (ConnectivityManager) getActivity()
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		if (conMgr.getActiveNetworkInfo() != null
-				&& conMgr.getActiveNetworkInfo().isAvailable()
-				&& conMgr.getActiveNetworkInfo().isConnected()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 }
