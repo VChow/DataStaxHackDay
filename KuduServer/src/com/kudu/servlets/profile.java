296e9477503084cab32129c7d1f2b07c6e54b1f3
mmgoddard <mmgoddard@dundee.ac.uk>
2014-03-23 12:08:36 +0000

    fixed errors with profile class

--- KuduServer/src/com/kudu/servlets/profile.java
+++ KuduServer/src/com/kudu/servlets/profile.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import org.json.JSONArray;
 //import org.json.JSONArray;
 import org.json.simple.JSONObject;
 
