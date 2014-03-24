f0d80f37dd84957ed5ae96519296b7d5bea1bd74
tbutterwith <tbutterwith@gmail.com>
2014-03-24 22:23:02 +0000

    fixed conversation overview, my bad guys

--- kudu/src/com/kudu/activities/ConversationActivityFragment.java
+++ kudu/src/com/kudu/activities/ConversationActivityFragment.java
@@ -1,12 +1,33 @@
 package com.kudu.activities;
 
-import android.app.Fragment;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import org.json.JSONException;
+
+import com.kudu.models.ConversationOverviewModel;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.ListFragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ConversationActivityFragment extends Fragment {
+import com.kudu.models.ConversationOverviewModel;
+
+public class ConversationActivityFragment extends ListFragment {
+String username = "admin";
+private Context context;
 
     public ConversationActivityFragment() {}
 
@@ -14,9 +35,90 @@ public class ConversationActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.conversation_activity_fragment, container, false);
-        
-        //test to see if the fragment is changing on drawer click.
-        //getActivity().setTitle("ConversationActivityFragment");
+        context = container.getContext();
+        setHasOptionsMenu(true);
+        displayConversations();
         return rootView;
     }
+    
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+     super.onCreateOptionsMenu(menu, inflater);
+     inflater.inflate(R.menu.action_bar_icons_contacts, menu);
+    }
+    
+    private void displayConversations(){
+if(checkInternetConnection()) {
+new Thread(new Runnable() {
+public void run() {
+ConversationOverviewModel com = new ConversationOverviewModel();
+try {
+LinkedList<String> temp = new LinkedList<String>();
+Log.d("fuck", "About to get conversation list");
+temp = com.getConversations(username);
+
+Log.d("fuck", "Contents of temp: "+temp.get(0));
+Log.d("fuck", "Got Conversations List");
+
+
+int size = temp.size();
+String[] values = new String[size];
+
+for(int i = 0; i < temp.size(); i++)
+values[i] = temp.get(i);
+setArray(values, size);
+} catch (IllegalStateException e) {
+e.printStackTrace();
+} catch (IOException e) {
+e.printStackTrace();
+} catch (JSONException e) {
+e.printStackTrace();
+}
+}
+}).start();
+}
+}
+
+public void setArray(String[] array, int size) {
+     final String[] values = new String[size];
+     for(int i = 0; i < array.length; i++)
+     {values[i] = array[i];
+     Log.d("fuck", ""+values[i]);}
+     ((Activity)context).runOnUiThread(new Runnable(){
+public void run(){
+ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, values);
+setListAdapter(adapter);
+}
+});
+    }
+
+public void onListItemClick(ListView lv, View v, int position, long id) {
+     String itemSelection = getListView().getItemAtPosition(position).toString();
+     //new addDialog(getActivity(), itemSelection);
+    }
+
+/*
+* Needs changing ***
+*/
+public boolean onOptionsItemSelected(MenuItem item) {
+     switch(item.getItemId()) {
+     case R.id.add_contact:
+     DialogFragment newFragment = AddContactDialog.newInstance();
+     newFragment.show(getFragmentManager(), "dialog");
+     return true;
+     default:
+     return super.onOptionsItemSelected(item);
+     }
+    }
+
+private boolean checkInternetConnection() {
+ConnectivityManager conMgr = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+if (conMgr.getActiveNetworkInfo() != null
+&& conMgr.getActiveNetworkInfo().isAvailable()
+&& conMgr.getActiveNetworkInfo().isConnected()) {
+return true;
+} else {
+return false;
+}
+}
 }
